2b810d08ab648ba133c2585777a0067954d8e8a3
Tobias Kahn <tobiasbkahn@gmail.com>
2015-12-10 17:02:19 -0500

    Finish core requirements + some style pizazz

--- js/main.js
+++ js/main.js
@@ -1,66 +1,94 @@
 // Create an object containing user's names and score
-var playerScores = { "Tobias": 5, "Rob": 10, "Jon": 20, "Sansa": 35};
+var playerScores = [];
 
 $("#add").click(function(){
-	var newRanking = $('#rankInput').val();
+	// add validations, inputScore must be a number, inputName could be anything really
+	var inputName = $('#nameInput').val();
+	var inputScore = parseInt($('#scoreInput').val());
+	var playerPresent = false
 
-	// parse the input and check if the user's name is already present.
-	// Using a regexp is silly I should just make two separate text fields!
-	if ( newRanking.search(/., \d/) != -1 ) {
-
-		// assign everything before the , to name and everything after the comma to score
-		// Once again, two text fields will simplify this
-		indexOfComma = newRanking.indexOf(",");
-		var inputName = newRanking.slice(0, indexOfComma);
-		var inputScore = parseInt(newRanking.slice(indexOfComma + 2, newRanking.length));
-		var playerPresent = false
-
-		// check if the player is already in the collection
-		for (player in playerScores ) {
-				if (player == inputName) {
-		    	playerScores[player] += inputScore;
-		    	playerPresent = true;
-		    	break;
-				} else {
-					playerPresent = false;
-				}
-			};
-			
-		if (playerPresent == false) {
-			playerScores[inputName] = inputScore;
-		};
-
-
-		// Order objects in playerScores by score value
-		// I'm creating an array here, maybe I should just use an array to begin with?
-		var sortable = [];
-			for (player in playerScores){
-				sortable.push([player, playerScores[player]])
-				sortable.sort(function(a, b) {return b[1] - a[1]});
+	for (var i = 0; i < playerScores.length; i++) {
+		if (playerScores[i].name == inputName) {
+			playerScores[i].score += inputScore;
+			playerPresent = true;
+    	break;
+		} else {
+			playerPresent = false;
 		}
-
-
-		$("#rankings").empty();
-		$("#rankings").append(createListItemHTML(sortable));
-	
-	} else {
-		alert("Please input your score in the format: 'Name, score'" )
 	};
+		
+	if (playerPresent == false) {
+		playerScores.push({ name: inputName, score: inputScore })
+	};
+
+	// Order objects in playerScores by score value
+	playerScores.sort(function(a, b) {return b.score - a.score} );
+
+	setRank(playerScores);
+
+	$("#rankings").empty();
+	$("#rankings").append(createListItemHTML(playerScores));	 
 });
 
-// This needs to actually remove the data from playerScores 
+// remove #rankings and reset playerScores
 $("#clear").click(function(){
 	$("#rankings").empty();
-})
+	playerScores = {};
+});
 
 // Make a function to create each li
-// Can I refactor this to avoid creating var output?
 function createListItemHTML(sortedArray) {
 	var output = ""
+	var highScore = sortedArray[0].score
+
 	for (var i = 0; i < sortedArray.length; i++){
-		output += "<li>" + sortedArray[i][0] + " " + sortedArray[i][1] + "</li>" 
+		var score = sortedArray[i].score;
+		var playerName = sortedArray[i].name;
+		output += "<li style='width:" + getLiWidth(highScore, score) + "'><div class='playerText'>" + sortedArray[i].rank + ". " + playerName + " " + score + " " + getPointsText(score) + "</div></li>" 
 	}
 	return output
+};
+
+function getLiWidth(highestScore, playerScore) {
+    return (playerScore / highestScore) * 100 + "%"
+};
+
+function getPointsText(score) {
+	if (score == 1) {
+		return "Point"
+	} else {
+		return "Points"
+	};
+};
+
+function setRank(sortedArray) {
+	var counter = 1;
+  var arrayLength = sortedArray.length;
+  var i = 0; 
+
+  if (sortedArray.length > 1) {
+	  while (i < arrayLength - 1) {
+	    if (sortedArray[i].score > sortedArray[i + 1].score) {
+	      sortedArray[i]["rank"] = counter;
+	      counter++;
+	      i++
+	    } else {
+	      sortedArray[i]["rank"] = counter;
+	      i++
+	    }
+	  };
+	  
+	  // Assign a rank to the last item in the array
+	  if (sortedArray[arrayLength - 1].score < sortedArray[arrayLength - 2].score) {
+	  	sortedArray[arrayLength - 1].rank = counter;
+	  } else {
+	    sortedArray[arrayLength - 1].rank = counter; 
+	  };
+	 } else {
+	 	sortedArray[0]["rank"] = 1;
+	 };
+
+  return sortedArray
 };
 
 
