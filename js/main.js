d6c32d2d8f9a246f0fe7ac4045b53849923e5124
Tobias Kahn <tobiasbkahn@gmail.com>
2015-12-13 20:55:32 -0500

    Styling cleanup and fixes. Ranking system now correct, I had misunderstood the requirements. Unfortunately no animations as that would need a drastic rewrite of the project.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,40 @@
-// Create an object containing user's names and score
 var playerScores = [];
+var inputName = "";
+var inputScore = 0;
 
 $("#add").click(function(){
-	// add validations, inputScore must be a number, inputName could be anything really
-	var inputName = $('#nameInput').val();
-	var inputScore = parseInt($('#scoreInput').val());
-	var playerPresent = false
+	var newRanking = $('#rankInput').val();
 
-	for (var i = 0; i < playerScores.length; i++) {
-		if (playerScores[i].name == inputName) {
-			playerScores[i].score += inputScore;
-			playerPresent = true;
-    	break;
-		} else {
-			playerPresent = false;
-		}
-	};
-		
-	if (playerPresent == false) {
-		playerScores.push({ name: inputName, score: inputScore })
+	if ( newRanking.search(/., \d/) != -1 ) {
+		indexOfComma = newRanking.indexOf(",");
+		inputName = newRanking.slice(0, indexOfComma);
+		inputScore = newRanking.slice(indexOfComma + 2, newRanking.length);
+	} else {
+		alert("Please enter your score in the format: 'Name, score'");
 	};
 
-	// Order objects in playerScores by score value
-	playerScores.sort(function(a, b) {return b.score - a.score} );
-
-	setRank(playerScores);
-
-	$("#rankings").empty();
-	$("#rankings").append(createListItemHTML(playerScores));	 
+	if (validate() == true) {
+		var data = createListItemHTML(addNewScore(inputName, inputScore));
+		$("#rankings").empty();
+		$("#rankings").append(data);
+		$("#clear").css("display", "inline")
+	}
+	emptyInputField();
 });
 
 // remove #rankings and reset playerScores
 $("#clear").click(function(){
+	emptyInputField();
+	$("#clear").css("display", "none")
 	$("#rankings").empty();
-	playerScores = {};
+	playerScores = [];
 });
 
-// Make a function to create each li
+function emptyInputField() {
+	$('#rankInput').val("");
+};
+
+// Create each li
 function createListItemHTML(sortedArray) {
 	var output = ""
 	var highScore = sortedArray[0].score
@@ -44,7 +42,7 @@ function createListItemHTML(sortedArray) {
 	for (var i = 0; i < sortedArray.length; i++){
 		var score = sortedArray[i].score;
 		var playerName = sortedArray[i].name;
-		output += "<li style='width:" + getLiWidth(highScore, score) + "'><div class='playerText'>" + sortedArray[i].rank + ". " + playerName + " " + score + " " + getPointsText(score) + "</div></li>" 
+		output += "<li style='width: " + getLiWidth(highScore, score) + "'><div class='playerText'>" + sortedArray[i].rank + ". " + playerName + ", " + score + " " + getPointsText(score) + "</div></li>"
 	}
 	return output
 };
@@ -62,33 +60,70 @@ function getPointsText(score) {
 };
 
 function setRank(sortedArray) {
-	var counter = 1;
-  var arrayLength = sortedArray.length;
-  var i = 0; 
+  var score = 0;
+  var i = 0;
+  var prevPosition = 1
 
-  if (sortedArray.length > 1) {
-	  while (i < arrayLength - 1) {
-	    if (sortedArray[i].score > sortedArray[i + 1].score) {
-	      sortedArray[i]["rank"] = counter;
-	      counter++;
-	      i++
-	    } else {
-	      sortedArray[i]["rank"] = counter;
-	      i++
-	    }
-	  };
-	  
-	  // Assign a rank to the last item in the array
-	  if (sortedArray[arrayLength - 1].score < sortedArray[arrayLength - 2].score) {
-	  	sortedArray[arrayLength - 1].rank = counter;
-	  } else {
-	    sortedArray[arrayLength - 1].rank = counter; 
-	  };
-	 } else {
-	 	sortedArray[0]["rank"] = 1;
-	 };
+	while (i < sortedArray.length) {
+      if (sortedArray[i].score == score) {
+          sortedArray[i].rank = prevPosition;
+      } else {
+          sortedArray[i].rank = i + 1;
+          score = sortedArray[i].score;
+          prevPosition = i + 1;
+      }
+			i++;
+	};
+  return sortedArray;
+};
 
-  return sortedArray
+function addNewScore(inputName, inputScore) {
+var playerPresent = false;
+
+	for (var i = 0; i < playerScores.length; i++) {
+		if (playerScores[i].name == inputName) {
+			playerScores[i].score += inputScore;
+			playerPresent = true;
+    	break;
+		} else {
+			playerPresent = false;
+		}
+	};
+
+	if (playerPresent == false) {
+		playerScores.push({ name: inputName, score: inputScore })
+	};
+
+	playerScores.sort(function(a, b) {return b.score - a.score} );
+
+	return setRank(playerScores);
 };
 
 
+function validate() {
+
+	function nameValidation() {
+		if (inputName == " ") {
+				alert("Please enter in a player name. It can be anything except for a blank space.");
+				return false;
+			} else {
+				return true;
+			}
+		};
+
+	function scoreValidation() {
+		if (parseInt(inputScore) == inputScore) {
+			inputScore = parseInt(inputScore); // This solves for "10.0" == 10 edge case
+			return true
+		} else {
+			alert("Score must be a whole number");
+			return false;
+		}
+	};
+
+	if (scoreValidation() == true && nameValidation() == true) {
+		return true
+	} else {
+		return false
+	}
+};
